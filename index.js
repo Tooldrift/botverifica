052a7995d5a527a778ca1515d86e1d02289191e4
ToolDrift <64321727+Tooldrift@users.noreply.github.com>
2022-10-24 21:40:04 +0200

    cc

--- index.js
+++ index.js
@@ -1,110 +1,47 @@
-const { prefix, developerID } = require("./config.json")
-const { config } = require("dotenv");
-const db =require("quick.db");
-const welc = require("image-cord")
-const Discord = require('discord.js')
-const express = require('express')
-const { Client, MessageEmbed }  = require('discord.js');
-const client = new Discord.Client({
-  disableEveryone: false
+const { Collection, Client, Discord, MessageEmbed, Message } = require('discord.js');
+const client = new Client({
+    disableMention: 'everyone'
 });
-let cooldown = new Set();
-let cdseconds = 1; // cooldown time
+require('discord-buttons')(client);
+const path = require('path')
+const fs = require('fs')
+const discordbuttons = require('discord-buttons')
+const { MessageButton, MessageActionRow } = require("discord-buttons")
+const keepAlive = require("./server");
+const config = require('./config.json');
+client.prefix = config.prefix;
 
-// This code is made by Supreme#2401
+client.on('clickButton', async (button) => {
+    if (button.id == 'AddVerifiedRole') {
+        button.reply.send(`You have been verified!`, true)
+        const role = button.guild.roles.cache.get(config.roleid)
+        const member = button.clicker.member
+        await member.roles.add(role)
+    }
+})
 
+client.on('ready', () => {
+    console.log('BOT IS ONLINE CREDIS- CODING WITH THUNDER')
+})
 
-client.commands = new Discord.Collection();
-client.aliases = new Discord.Collection();
+client.on('message', async (message) => {
+    if (message.content.startsWith('.verify')) {
+        const embed = new MessageEmbed()
+            .setTitle('Verification')
+            .setColor("GREEN")
+            .setDescription('Click on the button below to verify!')
 
-["command"].forEach(handler => {
-  require(`./handlers/${handler}`)(client);
-});
-process.on('UnhandledRejection', console.error);
- 
+        const add = new MessageButton()
+            .setStyle("green")
+            .setLabel("Verify Me!")
+            .setID("AddVerifiedRole")
 
-client.on("message", async message => {
-    
-  if (message.author.bot) return;
-  if (!message.guild) return;
-  if (!message.content.startsWith(prefix)) return;
-
-  if(cooldown.has(message.author.id)){
-
-    return message.channel.send(`**${message.author.username}** wait \`10\` seconds to use this command!`)
-  }
-  cooldown.add(message.author.id);
-  setTimeout(() => {
-cooldown.delete(message.author.id)}, cdseconds * 1000)
-
-  if (!message.member)
-    message.member = message.guild.fetchMember(message);
-
-
-  const args = message.content
-    .slice(prefix.length)
-    .trim()
-    .split(/ +/g);
-  const cmd = args.shift().toLowerCase();
-
-  if (cmd.length === 0) return;
-
-  let command = client.commands.get(cmd);
-
-  if (!command) command = client.commands.get(client.aliases.get(cmd));
-
-  if (command) command.run(client, message, args);  
-
-});
+        const row = new MessageActionRow()
+            .addComponent([add])
 
 
-client.on("message", async message => {
-
-const channel = db.get(`verify_${message.guild.id}`);
-
-if (channel === null) {
-    return;
-  }
-
-const chan = client.channels.cache.get(channel);
-if (message.channel.name == chan.name) {
-  message.delete();
-
-}
-
-        
-});
-
-
-
-
-
-// Do not change anything here
-require('http').createServer((req, res) => res.end(`
- |-----------------------------------------|
- |              Informations               |
- |-----------------------------------------|
- |• Alive: 24/7                            |
- |-----------------------------------------|
- |• Author:[18th] Fʅყιɳɠ KαႦιɳ!#330        |
- |---------------------------------------- |
- |• Server: https://dsc.gg/logistack       |
- |-----------------------------------------|
- |• Github: https://github.com/Kabin1234   |
- |-----------------------------------------|
- |• License: Apache License 2.0            |
- |-----------------------------------------|
-`)).listen(3000) //Dont remove this 
-
-client.on("ready", () => {
-   client.user.setStatus("dnd"); // You can change it to online, dnd, idle
-
- console.log(`Successfully logined as ${client.user.tag} `)
-});
-
-//  For Watching Status
-// client.on("ready", () => {
-// client.user.setActivity(`Chilling with owner`, { type:         "STREAMING",
-// url: "https://www.twitch.tv/nocopyrightsounds"})});
-
+        message.channel.send({ component: row, embed: embed })
+    }
+})
+keepAlive();
 client.login(process.env.TOKEN);
